1defe2adc6d898ba08b842e4185c1cfec245edc7
Stanislau Khomich 550503 <stanislaukhomich@gmail.com>
2017-11-16 00:16:43 +0300

    added forms

--- music/views.py
+++ music/views.py
@@ -3,8 +3,8 @@
 from django.http import Http404
 #from django.http import HttpResponse, request
 #from django.template import loader
-from django.shortcuts import render
-from .models import Album
+from django.shortcuts import render, get_object_or_404
+from .models import Album, Song
 
 def index(request):
 
@@ -24,10 +24,25 @@ def index(request):
     return render(request, 'music/index.html', {'all_albums': all_albums})
 
 def detail(request, album_id):
-    try:
+    """try:
         album = Album.objects.get(pk=album_id)
     except Album.DoesNotExist:
-        raise Http404("ALbum doesn't exist")
+        raise Http404("ALbum doesn't exist")"""
+    album = get_object_or_404(Album, pk=album_id)
     return render(request, 'music/detail.html', {'album': album})
+
+def favorite(request, album_id):
+    album = get_object_or_404(Album, pk=album_id)
+    try:
+        selected_song = album.song_set.get(pk=request.POST['song'])
+    except(KeyError, Song.DoesNotExist):
+        return render(request, 'music/detail.html', {
+            'album': album,
+            'error_message': "You didn't select a valid song",
+        })
+    else:
+        selected_song.is_favorite = True
+        selected_song.save()
+        return render(request, 'music/detail.html', {'album': album})
 
     #return HttpResponse("<h2>Details for Album id: " + str(album_id) + " </h2>")
