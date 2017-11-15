541989c4f7b4a8a002904184d07c176928236bbd
Stanislau Khomich 550503 <stanislaukhomich@gmail.com>
2017-11-15 14:21:02 +0300

    added html templates

--- music/views.py
+++ music/views.py
@@ -1,8 +1,34 @@
 from django.shortcuts import render
 
 # Create your views here.
-
-from django.http import HttpResponse
+from django.http import Http404
+from django.http import HttpResponse, request
+#from django.template import loader
+from django.shortcuts import render
+from .models import Album
 
 def index(request):
-    return HttpResponse("<h1>This is the music app homepage</h1>")
+
+    """html = ''
+    for album in all_albums:
+        url = '/music/' + str(album.id) + '/'
+        html += '<a href="' + url + '">' + album.album_title + '</a><br>'
+    all_albums = Album.objects.all()
+    template = loader.get_template('music/index.html')
+    context = {
+        'all_albums': all_albums,
+    }
+    return HttpResponse(template.render(context, request)) #(^-^) """
+
+    all_albums =Album.objects.all()
+    context = {'all_albums': all_albums}
+    return render(request, 'music/index.html', context)
+
+def detail(request, album_id):
+    try:
+        album = Album.objects.get(pk=album.id)
+    except Album.DoesNotExist:
+        raise Http404("ALbum doesn't exist")
+        return render(request, 'music/index.html', {'album': album})
+
+    #return HttpResponse("<h2>Details for Album id: " + str(album_id) + " </h2>")
